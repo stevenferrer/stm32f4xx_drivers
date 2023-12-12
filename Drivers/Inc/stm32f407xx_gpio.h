d6f5cb1a5f199fdafccbe7f8d92a38d5b8774fc3
Steven Ferrer <steven.r.ferrer@gmail.com>
2023-12-12 20:41:55 +0800

    Added GPIO API prototypes

--- Drivers/Inc/stm32f407xx_gpio.h
+++ Drivers/Inc/stm32f407xx_gpio.h
@@ -22,4 +22,17 @@ typedef struct GPIO_Handle_t {
 	GPIO_PinConfig_t GPIO_PinConfig;
 } GPIO_Handle_t;
 
+void GPIO_Init(void);
+void GPIO_DeInit(void);
+void GPIO_PClockCtrl(void);
+
+void GPIO_ReadInputPin(void);
+void GPIO_ReadInputPort(void);
+void GPIO_WriteOutputPin(void);
+void GPIO_WriteOutputPort(void);
+void GPIO_ToggleOutputPin(void);
+
+void GPIO_IRQConfig(void);
+void GPIO_IRQHandling(void);
+
 #endif /* INC_STM32F407XX_GPIO_H_ */
