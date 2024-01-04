8fe772445a22333a7a02555ba4e90076292de892
Steven Ferrer <steven.r.ferrer@gmail.com>
2024-01-04 11:15:03 +0800

    Implement GPIO peripheral clock control API

--- Drivers/Src/stm32f407xx_gpio_driver.c
+++ Drivers/Src/stm32f407xx_gpio_driver.c
@@ -3,5 +3,91 @@
  *
  */
 
-
 #include <stm32f407xx_gpio_driver.h>
+
+/*
+ * @fn
+ *
+ * @breif
+ *
+ * @param[in]
+ * @param[in]
+ * @param[in]
+ *
+ * @return
+ *
+ * @note
+ */
+void GPIO_Init(GPIO_Handle_t *pGPIOHandle) {
+}
+
+void GPIO_PeriClockCtrl(GPIO_RegDef_t *pGPIOx, uint8_t enable) {
+	if (enable == ENABLE) {
+		if (pGPIOx == GPIOA) {
+			GPIOA_PCLK_EN();
+		} else if (pGPIOx == GPIOB) {
+			GPIOB_PCLK_EN();
+		} else if (pGPIOx == GPIOC) {
+			GPIOC_PCLK_EN();
+		} else if (pGPIOx == GPIOD) {
+			GPIOD_PCLK_EN();
+		} else if (pGPIOx == GPIOE) {
+			GPIOE_PCLK_EN();
+		} else if (pGPIOx == GPIOF) {
+			GPIOF_PCLK_EN();
+		} else if (pGPIOx == GPIOG) {
+			GPIOG_PCLK_EN();
+		} else if (pGPIOx == GPIOH) {
+			GPIOH_PCLK_EN();
+		} else if (pGPIOx == GPIOI) {
+			GPIOI_PCLK_EN();
+		}
+	} else {
+		if (pGPIOx == GPIOA) {
+			GPIOA_PCLK_DI();
+		} else if (pGPIOx == GPIOB) {
+			GPIOB_PCLK_DI()();
+		} else if (pGPIOx == GPIOC) {
+			GPIOC_PCLK_DI();
+		} else if (pGPIOx == GPIOD) {
+			GPIOD_PCLK_DI();
+		} else if (pGPIOx == GPIOE) {
+			GPIOE_PCLK_DI();
+		} else if (pGPIOx == GPIOF) {
+			GPIOF_PCLK_DI();
+		} else if (pGPIOx == GPIOG) {
+			GPIOG_PCLK_DI();
+		} else if (pGPIOx == GPIOH) {
+			GPIOH_PCLK_DI();
+		} else if (pGPIOx == GPIOI) {
+			GPIOI_PCLK_DI();
+		}
+	}
+}
+
+void GPIO_DeInit(GPIO_Handle_t *pGPIOHandle) {
+}
+
+uint8_t GPIO_ReadInputPin(GPIO_RegDef_t *pGPIOx, uint8_t pinNumber) {
+	return 0;
+}
+
+uint16_t GPIO_ReadInputPort(GPIO_RegDef_t *pGPIOx) {
+	return 0;
+}
+
+void GPIO_WriteOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t pinNumber,
+		uint8_t value) {
+}
+
+void GPIO_WriteOutputPort(GPIO_RegDef_t *pGPIOx, uint16_t value) {
+}
+
+void GPIO_ToggleOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t pinNumber) {
+}
+
+void GPIO_IRQConfig(uint8_t irqNumber, uint8_t irqPriority, uint8_t enable) {
+}
+
+void GPIO_IRQHandling(uint8_t pinNumber) {
+}
