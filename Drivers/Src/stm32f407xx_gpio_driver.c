4ab78291819ba308ca9576cd213920940758ba85
Steven Ferrer <steven.r.ferrer@gmail.com>
2024-01-07 16:17:09 +0800

    Fixed peripheral base addresses

--- Drivers/Src/stm32f407xx_gpio_driver.c
+++ Drivers/Src/stm32f407xx_gpio_driver.c
@@ -6,25 +6,49 @@
 #include <stm32f407xx_gpio_driver.h>
 
 void GPIO_PeriClockCtrl(GPIO_RegDef_t *pGPIOx, uint8_t enable) {
-	if (pGPIOx == GPIOA) {
-		enable == ENABLE ? GPIOA_PCLK_EN() : GPIOA_PCLK_DI();
-	} else if (pGPIOx == GPIOB) {
-		enable == ENABLE ? GPIOB_PCLK_EN() : GPIOB_PCLK_DI();
-	} else if (pGPIOx == GPIOC) {
-		enable == ENABLE ? GPIOC_PCLK_EN() : GPIOC_PCLK_DI();
-	} else if (pGPIOx == GPIOD) {
-		enable == ENABLE ? GPIOD_PCLK_EN() : GPIOD_PCLK_DI();
-	} else if (pGPIOx == GPIOE) {
-		enable == ENABLE ? GPIOE_PCLK_EN() : GPIOE_PCLK_DI();
-	} else if (pGPIOx == GPIOF) {
-		enable == ENABLE ? GPIOF_PCLK_EN() : GPIOF_PCLK_DI();
-	} else if (pGPIOx == GPIOG) {
-		enable == ENABLE ? GPIOG_PCLK_EN() : GPIOG_PCLK_DI();
-	} else if (pGPIOx == GPIOH) {
-		enable == ENABLE ? GPIOH_PCLK_EN() : GPIOH_PCLK_DI();
-	} else if (pGPIOx == GPIOI) {
-		enable == ENABLE ? GPIOI_PCLK_EN() : GPIOI_PCLK_DI();
+	if (enable == ENABLE) {
+		if (pGPIOx == GPIOA) {
+			 uint32_t tmp = 1 << 0;
+			 RCC->AHB1ENR |= tmp;
+		} else if (pGPIOx == GPIOB) {
+			GPIOB_PCLK_EN();
+		} else if (pGPIOx == GPIOC) {
+			GPIOC_PCLK_EN();
+		} else if (pGPIOx == GPIOD) {
+			GPIOD_PCLK_EN();
+		} else if (pGPIOx == GPIOE) {
+			GPIOE_PCLK_EN();
+		} else if (pGPIOx == GPIOF) {
+			GPIOF_PCLK_EN();
+		} else if (pGPIOx == GPIOG) {
+			GPIOG_PCLK_EN();
+		} else if (pGPIOx == GPIOH) {
+			GPIOH_PCLK_EN();
+		} else if (pGPIOx == GPIOI) {
+			GPIOI_PCLK_EN();
+		}
+	} else {
+		if (pGPIOx == GPIOA) {
+			GPIOA_PCLK_DI();
+		} else if (pGPIOx == GPIOB) {
+			GPIOB_PCLK_DI();
+		} else if (pGPIOx == GPIOC) {
+			GPIOC_PCLK_DI();
+		} else if (pGPIOx == GPIOD) {
+			GPIOD_PCLK_DI();
+		} else if (pGPIOx == GPIOE) {
+			GPIOE_PCLK_DI();
+		} else if (pGPIOx == GPIOF) {
+			GPIOF_PCLK_DI();
+		} else if (pGPIOx == GPIOG) {
+			GPIOG_PCLK_DI();
+		} else if (pGPIOx == GPIOH) {
+			GPIOH_PCLK_DI();
+		} else if (pGPIOx == GPIOI) {
+			GPIOI_PCLK_DI();
+		}
 	}
+
 }
 
 /*
@@ -122,7 +146,7 @@ uint16_t GPIO_ReadInputPort(GPIO_RegDef_t *pGPIOx) {
 void GPIO_WriteOutputPin(GPIO_RegDef_t *pGPIOx, uint8_t pinNumber,
 		uint8_t value) {
 
-	if (value == GPIO_PIN_SET) {
+	if (value == SET) {
 		// write 1 to ODR at pin bit field
 		pGPIOx->ODR |= (0x1 << pinNumber);
 
