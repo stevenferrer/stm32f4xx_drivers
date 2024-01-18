2790bceaf34e2121310eaa5e40bb9d99b1cb64c2
Steven Ferrer <steven.r.ferrer@gmail.com>
2024-01-18 18:27:06 +0800

    Added SPI API prototypes

--- Drivers/Inc/stm32f407xx_spi_driver.h
+++ Drivers/Inc/stm32f407xx_spi_driver.h
@@ -25,4 +25,29 @@ typedef struct SPI_Handle_t {
 	SPI_Config_t config;
 } SPI_Handle_t;
 
+/*
+ * Peripheral clock setup
+ */
+
+void SPI_PeriClockCtrl(SPI_RegDef_t *pSPIx, uint8_t enable);
+
+/*
+ * Init and de-init
+ */
+void SPI_Init(SPI_Handle_t *pSPIHandle);
+void SPI_DeInit(SPI_RegDef_t *pSPIx);
+
+/*
+ * Data send and receive
+ */
+void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t size);
+void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t size);
+
+/*
+ * IRQ config and ISR handling
+ */
+void SPI_IRQInterruptConfig(uint8_t irqNumber, uint8_t enable);
+void SPI_IRQPriorityConfig(uint8_t irqNumber, uint8_t irqPriority);
+void SPI_IRQHandling(SPI_Handle_t *pSPIHandle);
+
 #endif /* INC_STM32F407XX_SPI_DRIVER_H_ */
--- Drivers/Src/stm32f407xx_spi_driver.c
+++ Drivers/Src/stm32f407xx_spi_driver.c
@@ -5,5 +5,37 @@
  *      Author: sf
  */
 
-
 #include "stm32f407xx_spi_driver.h";
+
+/*
+ * Peripheral clock setup
+ */
+void SPI_PeriClockCtrl(SPI_RegDef_t *pSPIx, uint8_t enable) {
+
+}
+
+/*
+ * Init and de-init
+ */
+void SPI_Init(SPI_Handle_t *pSPIHandle) {
+}
+void SPI_DeInit(SPI_RegDef_t *pSPIx) {
+}
+
+/*
+ * Data send and receive
+ */
+void SPI_SendData(SPI_RegDef_t *pSPIx, uint8_t *pTxBuffer, uint32_t size) {
+}
+void SPI_ReceiveData(SPI_RegDef_t *pSPIx, uint8_t *pRxBuffer, uint32_t size) {
+}
+
+/*
+ * IRQ config and ISR handling
+ */
+void SPI_IRQInterruptConfig(uint8_t irqNumber, uint8_t enable) {
+}
+void SPI_IRQPriorityConfig(uint8_t irqNumber, uint8_t irqPriority) {
+}
+void SPI_IRQHandling(SPI_Handle_t *pSPIHandle) {
+}
