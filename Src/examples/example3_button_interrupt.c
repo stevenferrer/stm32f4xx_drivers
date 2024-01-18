267b9c7b735e6b6d83f72b316e0615e86b7210e4
Steven Ferrer <steven.r.ferrer@gmail.com>
2024-01-18 15:42:07 +0800

    Update button interrupt example

--- Src/examples/example3_button_interrupt.c
+++ Src/examples/example3_button_interrupt.c
@@ -15,28 +15,36 @@
 #include "../utils.h"
 
 void button_interrupt(void) {
-	GPIO_Handle_t gpio_led, gpio_btn;
+	GPIO_Handle_t gpio_led1, gpio_led2, gpio_btn;
 	// initialize every member to zero
-	memset(&gpio_led, 0, sizeof(gpio_led));
+	memset(&gpio_led1, 0, sizeof(gpio_led1));
 	memset(&gpio_btn, 0, sizeof(gpio_btn));
 
+	GPIO_PeriClockCtrl(GPIOA, ENABLE);
 	GPIO_PeriClockCtrl(GPIOD, ENABLE);
 
-	gpio_led.pGPIOx = GPIOD;
-	gpio_led.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
-	gpio_led.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
-	gpio_led.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
-	gpio_led.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
-	gpio_led.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPD_NO;
+	gpio_led1.pGPIOx = GPIOD;
+	gpio_led1.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_12;
+	gpio_led1.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	gpio_led1.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	gpio_led1.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	gpio_led1.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPD_NO;
+	GPIO_Init(&gpio_led1);
 
-	GPIO_Init(&gpio_led);
+	gpio_led2.pGPIOx = GPIOA;
+	gpio_led2.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_1;
+	gpio_led2.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
+	gpio_led2.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
+	gpio_led2.GPIO_PinConfig.GPIO_PinOPType = GPIO_OP_TYPE_PP;
+	gpio_led2.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPD_NO;
+	GPIO_Init(&gpio_led2);
+	GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_1);
 
 	gpio_btn.pGPIOx = GPIOD;
 	gpio_btn.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_5;
 	gpio_btn.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_IT_FT;
 	gpio_btn.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
 	gpio_btn.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_PUPD_PU;
-
 	GPIO_Init(&gpio_btn);
 
 	GPIO_IRQPriorityConfig(IRQ_NO_EXTI9_5, NVIC_IRQ_PRIORITY_15);
@@ -52,5 +60,6 @@ void EXTI9_5_IRQHandler(void) {
 	GPIO_IRQHandling(GPIO_PIN_NO_5);
 	delay(3);
 	GPIO_ToggleOutputPin(GPIOD, GPIO_PIN_NO_12);
+	GPIO_ToggleOutputPin(GPIOA, GPIO_PIN_NO_1);
 }
 
